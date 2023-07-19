d936e463cd478c53ebf08eb846ea9d9a9baacd54
mdafraz <azimafraz@gmail.com>
2023-07-20 02:53:45 +0530

    completed File Metadata Microservice

--- File-Metadata/index.js
+++ File-Metadata/index.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const bodyParser = require("body-parser");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const file = req.file;
+  res.send({ name: file.originalname, type: file.mimetype, size: file.size });
 });
-
-
-
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
